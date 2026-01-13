3a22e105f758615d6e157f7cf1612f3278eb4d8b
Juanjo Tortosa <juanjo.tortosa@gmail.com>
2026-01-13 11:58:45 +0100

    fix(contract-ids): Add picpay contract_id property
    The LetPayContractIds class was missing the picpay property,
    causing empty contract_id to be sent in PicPay payment requests.
    
    Also added default empty values to all properties to prevent
    errors when some contract_ids are not configured.

--- src/parameters/LetPayContractIds.php
+++ src/parameters/LetPayContractIds.php
@@ -4,13 +4,14 @@ namespace LetPay\parameters;
 
 class LetPayContractIds
 {
-    public string $pix;
-    public string $boleto;
-    public string $oxxo;
-    public string $paycash;
-    public string $spei;
-    public string $paynet;
-    public string $bankTransfer;
+    public string $pix = '';
+    public string $boleto = '';
+    public string $oxxo = '';
+    public string $paycash = '';
+    public string $spei = '';
+    public string $paynet = '';
+    public string $picpay = '';
+    public string $bankTransfer = '';
 
     public function __construct(object $params)
     {
